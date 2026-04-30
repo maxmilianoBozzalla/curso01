9a9fa6ceb7224c378894a58b4006394a9753482e
maxmilianoBozzalla <maximiliano.bozzalla@gmail.com>
2026-04-30 07:35:23 -0300

    Initial commit - proyecto base Spring Boot

---
+++ src/main/java/api_curso01/Curso01Application.java
@@ -0,0 +1,13 @@
+package api_curso01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso01Application.class, args);
+	}
+
+}
---
+++ src/test/java/api_curso01/Curso01ApplicationTests.java
@@ -0,0 +1,13 @@
+package api_curso01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
